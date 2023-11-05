2e2b614c0ab94d22e35a38944b6daa828aec0ccf
SHAVITARAINA <147043438+SHAVITARAINA@users.noreply.github.com>
2023-11-05 19:58:54 +0530

    Add files via upload

---
+++ reverse.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+int main() {
+    int num = 10; // Starting point (you can change this value)
+    
+    while (num >= 1) {
+        printf("%d\n", num);
+        num--;
+    }
+    
+    return 0;
+}
